3baaa1033ce1136e51429e019d667e128a0a84c4
omerr0542 <o.faruk0542@gmail.com>
2025-06-19 21:16:52 +0300

    Soyut sınıflar ve generic repository eklendi

---
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface ICategoryDal : IRepository<Category> 
+    {
+        // Bunun yerine IRepository<Category> kullanabilirdik ama bu şekilde de yazabiliriz. Best Practice IRepository
+        //List<Category> List();
+        //void Insert(Category p);
+        //void Update(Category p);
+        //void Delete(Category p);
+    }
+}
---
+++ DataAccessLayer/Concrete/Repositories/GenericRepository.cs
@@ -0,0 +1,57 @@
+﻿using DataAccessLayer.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    // T değeri class olmak zorunda ve referans tip olmalı
+    public class GenericRepository<T> : IRepository<T> where T: class
+    {
+        Context c = new Context();
+        DbSet<T> _object;
+
+        public GenericRepository()
+        {
+            _object = c.Set<T>(); // Context sınıfından gelen DbSet<T> tipindeki nesneyi _object değişkenine atıyoruz.
+                                  // Bu sayede dışarıdan gelen entity sınıflarını GenericRepository içinde kullanabiliyoruz.
+        }
+
+        public void Delete(T p)
+        {
+            _object.Remove(p);
+            c.SaveChanges();
+        }
+
+        public void Insert(T p)
+        {
+            _object.Add(p);
+            c.SaveChanges();
+        }
+
+        public List<T> List()
+        {
+            return _object.ToList();
+        }
+
+        public List<T> List(System.Linq.Expressions.Expression<Func<T, bool>> filter)
+        {
+            if (filter == null)
+            {
+                return _object.ToList(); // Eğer filtre yoksa tüm verileri listele
+            }
+            else
+            {
+                return _object.Where(filter).ToList(); // Filtre varsa, filtreye uyan verileri listele
+            }
+        }
+
+        public void Update(T p)
+        {
+            c.SaveChanges(); // Değişiklikleri kaydet
+        }
+    }
+}
