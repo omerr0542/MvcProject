89d0623d84789c7b9472eea8b82b7f05ba8dd2e8
omerr0542 <o.faruk0542@gmail.com>
2025-06-19 18:49:58 +0300

    get

---
+++ EntityLayer/Concrete/Content.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Content
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+        public Writer Writer { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Heading.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    // Concrete klasörü altında somut sınıflar bulunur.
+    public class Heading
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Writer { get; set; }
+    }
+}
