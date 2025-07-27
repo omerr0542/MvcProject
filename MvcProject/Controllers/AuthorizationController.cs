dd6d29f70cffeb0443ab1c96cea89a947911e4a4
omerr0542 <o.faruk0542@gmail.com>
2025-07-27 23:22:03 +0300

    Admin Controller Added

---
+++ MvcProject/Controllers/AuthorizationController.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcProject.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+
+        private readonly IAdminService _adminService;
+
+        public AuthorizationController(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_adminService.GetAll());
+        }
+
+        [HttpGet]
+        public IActionResult AddAdmin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddAdmin(Admin admin)
+        {
+            _adminService.AdminAdd(admin);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditAdmin(int id)
+        {
+            var admin = _adminService.GetById(id);
+            return View(admin);
+        }
+
+        [HttpPost]
+        public IActionResult EditAdmin(Admin admin)
+        {
+            _adminService.AdminUpdate(admin);
+            return RedirectToAction("Index");
+        }
+    }
+}
