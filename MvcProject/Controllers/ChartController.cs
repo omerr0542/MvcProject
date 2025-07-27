d6b44d5a474b983556746b224f62c6f0208fde06
omerr0542 <o.faruk0542@gmail.com>
2025-07-27 21:56:31 +0300

    Chart Controller Added

---
+++ MvcProject/Controllers/ChartController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcProject.Controllers
+{
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryChart()
+        {
+            return Json(BlogList());
+        }
+
+        public List<Models.CategoryClass> BlogList()
+        {
+            var categories = new List<Models.CategoryClass>
+            {
+                new Models.CategoryClass { CategoryName = "Category 1", CategoryCount = 10 },
+                new Models.CategoryClass { CategoryName = "Category 2", CategoryCount = 20 },
+                new Models.CategoryClass { CategoryName = "Category 3", CategoryCount = 30 }
+            };
+            return categories;
+        }
+
+    }
+}
--- MvcProject/Controllers/ContentController.cs
+++ MvcProject/Controllers/ContentController.cs
@@ -18,6 +18,12 @@ namespace MvcProject.Controllers
             return View();
         }
 
+        public IActionResult GetAllContent(string p = "")
+        {
+            var contents = _contentManager.GetAllWithIncludes().ToList().Where(x => x.ContentValue.Contains(p));
+            return View(contents);
+        }
+
         public IActionResult ContentByHeading(int id)
         {
             var contents = _contentManager.GetListByHeadingID(id);
